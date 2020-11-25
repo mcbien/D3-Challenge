95b2907e87956cbbb03d677693e310e2bab08a5d
mcbien <mcbien1122@gmail.com>
2020-11-24 18:35:31 -0600

    Saving homework progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,20 @@
 // @TODO: YOUR CODE HERE!
 
 // Set SVG Dimensions
-var svgWidth = 1000;
+var svgWidth = 1500;
 var svgHeight = 750;
 
 // Define the chart's margins
 var chartMargin = {
-    top: 100,
-    right: 100,
-    bottom: 100,
-    left: 100
+    top: 75,
+    right: 200,
+    bottom: 200,
+    left: 350
 };
 
 // Define chart area dimensions
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right  // 1000 - 20 -20 = 960
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom // 750 - 20 - 20 = 710
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom
 
 // Select body and append the SVG area to it and set the dimensions
 var svg = d3
@@ -56,7 +56,56 @@ d3.csv("data.csv").then(function (healthData) {
 
     // Create bottomAxis and leftAxis variables used to create scales
     var bottomAxis = d3.axisBottom(xLinearScale);
-    var lefAxis = d3.axisLeft(yLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append an SVG chartGroup append circles for each datapoint
+    chartGroup.append("g")
+        .selectAll("dot")   // There are none
+        .data(healthData)   // Bind data
+        .enter()            // Change focus to new elements only
+        .append("circle")
+        .attr("cx", function (data) {
+            //console.log(data.smokes)
+            return xLinearScale(data.smokes);
+        })
+        .attr("cy", function (data) {
+            //console.log(data.income)
+            return yLinearScale(data.income);
+        })
+        .attr("r", 20)
+        .style("fill", "#69b3a2")
+        .classed("circles", true)
+
+    // Append text
+    charGroup.append("text")
+        .text(function (data) {
+            return data.abbr
+        })
+
+
+
+
+
+
+
+
+
+
+
+    // Append an SVG group and for bottom axis
+    chartGroup.append("g")
+        .classed("axis", true)
+        .call(leftAxis)
+
+    // Append an SVG group for bottpm axis and translate to bottom of page
+    chartGroup.append("g")
+        .classed("axis", true)
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
+
+
+
+
 
 
 
